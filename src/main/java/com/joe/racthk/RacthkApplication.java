2afac05584ac2b55860296d263a8f170fb482e09
Joel Ochieng <ochiengj22@gmail.com>
2024-03-09 18:47:02 +0300

    Email integration

--- src/main/java/com/joe/racthk/RacthkApplication.java
+++ src/main/java/com/joe/racthk/RacthkApplication.java
@@ -3,8 +3,10 @@ package com.joe.racthk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = "com.joe.racthk.rest;") // Replace with the package where your controllers are located
 @ComponentScan(basePackages = "com.joe.racthk.service;") // Replace with the package where your controllers are located
 public class RacthkApplication {
